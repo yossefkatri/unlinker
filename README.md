c213395d1f8e7663511b8c92d4c834d3dca5fe3d
yossefkatri <yossefkatri.com>
2021-08-22 16:09:20 +0300

    beginning

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # UnLinker
 Unlink an ELF executable file into objects file......
+tools that used:
+commands: hd & readelf
+c/c++ debbuging on vscode
+ 
--- unlinker.c
+++ unlinker.c
@@ -12,15 +12,16 @@ void show_hex(char * obj , size_t size)
 }
 int main(int argc, char* argv[])
 {
-    if(argc != 2)
+    /*if(argc != 2)
     {
         printf("you should put one parmeter\n");
         exit(1);
-    }
-    FILE* executable = fopen(argv[1],"rb");
+    }*/
+    //FILE* executable = fopen(argv[1],"rb");
+    FILE* executable = fopen("a.out","rb");
     Elf32_Ehdr elfheader;
     fread((void *) &elfheader,sizeof(Elf32_Ehdr),1,(FILE*)executable);
-    show_hex((char *) &elfheader , sizeof(Elf32_Ehdr));
+    show_hex((char *) &elfheader , sizeof(elfheader));
     fclose(executable);
     return 0;
 
