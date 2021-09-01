0a85f6df8d42bc9cec047c5b728d5f13c0311eb8
yossefkatri <yossefkatri.com>
2021-09-02 00:41:52 +0300

    now he can seperate between 32 and 64 files

--- unlinker.c
+++ unlinker.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <elf.h>
+#include <string.h>
 
 void show_hex(char * obj , size_t size)
 {   
@@ -19,9 +20,35 @@ int main(int argc, char* argv[])
     }*/
     //FILE* executable = fopen(argv[1],"rb");
     FILE* executable = fopen("a.out","rb");
-    Elf32_Ehdr elfheader;
-    fread((void *) &elfheader,sizeof(Elf32_Ehdr),1,(FILE*)executable);
-    show_hex((char *) &elfheader , sizeof(elfheader));
+    if(executable == NULL)
+    {
+        printf("Error: could not open file %s \n", argv[1]);
+        return -1;
+    }
+    char magic[5];
+    fgets(magic, sizeof(magic), executable);
+    if(magic[0] != 127 || strcmp(magic+1, "ELF"))
+    {
+        printf("Error: this file isn't a ELF file \n");
+        fclose(executable);
+        return -1;
+    }
+    int class = fgetc(executable);
+    if(class == 1)
+    {
+        printf("32-bits file \n");
+        //Elf32_Ehdr elfheader;
+        //fread((void *) &elfheader,sizeof(Elf32_Ehdr),1,(FILE*)executable);
+        // //show_hex((char *) &elfheader , sizeof(elfheader));
+    }
+    else if(class)
+    {
+        printf("64-bits file \n");
+    }
+    else
+    {
+        printf("none(invalid) \n");
+    }
     fclose(executable);
     return 0;
 
